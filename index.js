f2f6bb44220a480c537ad63bcaf7df99f77e0acc
David <Marin9737@gmail.com>
2024-01-22 02:55:13 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John',
+    role: 'Developer',
+    age: 30,
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+  module.exports = { employee, updateEmployeeWithKeyAndValue };
+
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  module.exports = { employee, destructivelyUpdateEmployeeWithKeyAndValue };
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+  module.exports = { employee, deleteFromEmployeeByKey };
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  module.exports = { employee, destructivelyDeleteFromEmployeeByKey };
